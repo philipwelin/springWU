c7d2dd44761bb09201263195b7c3c0946e404348
philipwelin <philip.welin@elev.ga.ntig.se>
2026-04-17 11:00:48 +0200

    Fredag Vecka 16
    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let layer = document.querySelector(".fade-layer");
-layer.addEventListener("click",showMenu)
+layer.addEventListener("click", showMenu);
 
 let button = document.querySelector(".menu-button");
-button.addEventListener("click",showMenu)
+button.addEventListener("click", showMenu);
 
-
-
-function showMenu(){
+function showMenu() {
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible");
-} 
+}
+
+
